be27bc8df49cbbcebfa524d54537554f3eaeb05d
Joel Graycar <jgraycar@berkeley.edu>
2016-06-21 20:06:13 -0700

    Fix members select on performance form, badge in member list

--- app/pods/performances/edit/route.js
+++ app/pods/performances/edit/route.js
@@ -5,6 +5,11 @@ export default Ember.Route.extend({
     return this.store.findRecord('performance', params.performance_id);
   },
 
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('members', this.store.findAll('member'));
+  },
+
   actions: {
     viewPerformance(performance) {
       this.transitionTo('performances.show', performance);
