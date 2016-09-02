50aa1312345179c14667ff5828b077ad00447ee1
Joel Graycar <jgraycar@berkeley.edu>
2016-09-02 07:49:06 -0700

    Change to paper-fabs for resource creation buttons

--- app/pods/performances/index/route.js
+++ app/pods/performances/index/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('performance');
-  }
+  },
+
+  actions: {
+    create() {
+      this.transitionTo('performances.new');
+    },
+  },
 });
--- app/pods/setups/index/route.js
+++ app/pods/setups/index/route.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('setup');
   },
+
+  actions: {
+    create() {
+      this.transitionTo('setups.new');
+    },
+  },
 });
