36558bd1814e9a4755d5c13667abf886884f070d
Joel Graycar <jgraycar@berkeley.edu>
2016-09-28 02:52:08 -0700

    Add rotation and slider to edit to instruments

--- app/models/instrument.js
+++ app/models/instrument.js
@@ -32,6 +32,12 @@ export default DS.Model.extend({
     return this.get('shape') === 'cross';
   }),
 
+  /**
+   * Returns the x coordinate to use for displaying the instrument
+   * on the stage SVG element. Templates are not required to have a
+   * defined X attribute. Since they are only shown when being edited,
+   * their x coordinate should be such that they are centered.
+   */
   xCoor: Ember.computed('x', 'template', 'centerX', function() {
     if (this.get('template')) {
       return this.get('centerX');
@@ -40,6 +46,12 @@ export default DS.Model.extend({
     return this.get('x');
   }),
 
+  /**
+   * Returns the y coordinate to use for displaying the instrument
+   * on the stage SVG element. Templates are not required to have a
+   * defined Y attribute. Since they are only shown when being edited,
+   * their y coordinate should be such that they are centered.
+   */
   yCoor: Ember.computed('y', 'template', 'centerY', function() {
     if (this.get('template')) {
       return this.get('centerY');
@@ -48,6 +60,10 @@ export default DS.Model.extend({
     return this.get('y');
   }),
 
+  /**
+   * Calculates the x coordinate that would cause the instrument to be
+   * centered in the SVG stage element, based on shape and size.
+   */
   centerX: Ember.computed('x', 'shape', 'width', function() {
     let adjustment = 0;
     if (this.get('isRectangle')) {
@@ -57,6 +73,10 @@ export default DS.Model.extend({
     return 400 - adjustment;
   }),
 
+  /**
+   * Calculates the y coordinate that would cause the instrument to be
+   * centered in the SVG stage element, based on shape and size.
+   */
   centerY: Ember.computed('y', 'shape', 'height', function() {
     let adjustment = 0;
     if (this.get('isRectangle')) {
@@ -64,5 +84,25 @@ export default DS.Model.extend({
     }
 
     return 225 - adjustment;
+  }),
+
+  /**
+   * Returns the SVG transformation to rotate the instrument ANGLE degrees.
+   */
+  rotationTransformation: Ember.computed(
+      'angle', 'xCoor', 'yCoor', 'shape', 'width', 'height', function() {
+    let pivotX = this.get('xCoor');
+    let pivotY = this.get('yCoor');
+
+    // xCoor and yCoor are adjusted so that no matter the instrument's shape,
+    // they will point to it's center (other than circle's, most SVG elements
+    // instead point to their top left corner). Undo that adjustment here so
+    // that the rotation applies as expected.
+    if (this.get('isRectangle')) {
+      pivotX += this.get('width') / 2;
+      pivotY += this.get('height') / 2;
+    }
+
+    return `rotate(${this.get('angle')} ${pivotX} ${pivotY})`;
   }),
 });
