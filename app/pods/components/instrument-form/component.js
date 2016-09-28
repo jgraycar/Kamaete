0e873647deeb248f6022db01adf38ac7bc031772
Joel Graycar <jgraycar@berkeley.edu>
2016-09-28 02:18:41 -0700

    Use mutable bindings instead of event handlers for sliders

--- app/pods/components/instrument-form/component.js
+++ app/pods/components/instrument-form/component.js
@@ -1,41 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didRender() {
-    this._super(...arguments);
-    const sliders = this.$('paper-slider');
-
-    const immediateValueChangeHandler = (e) => {
-      const attribute = e.srcElement.dataset.attribute;
-      this.get('instrument').set(attribute, e.srcElement.immediateValue);
-    };
-
-    const valueChangeHandler = (e) => {
-      const attribute = e.srcElement.dataset.attribute;
-      this.get('instrument').set(attribute, e.srcElement.value);
-    };
-
-    for (const slider of sliders) {
-      slider.addEventListener('immediate-value-change', immediateValueChangeHandler);
-      slider.addEventListener('change', valueChangeHandler);
-    }
-  },
-
-  actions: {
-    poop() {
-      debugger;
-    },
-
-    valueChange() {
-      debugger;
-    },
-
-    change() {
-      debugger;
-    },
-
-    immediateValueChange() {
-      debugger;
-    },
-  },
+  shapeOptions: ['ellipse', 'rectangle', 'cross', 'triangle'],
 });
