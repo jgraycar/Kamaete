ed289bce33e8848f810fc58eabe96381f05ff988
Joel Graycar <jgraycar@berkeley.edu>
2016-10-04 01:07:17 -0700

    Only show template instruments on index page

--- app/pods/instruments/index/route.js
+++ app/pods/instruments/index/route.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('instrument');
+    return this.store.findAll('instrument').then(
+      (instruments) => instruments.filter((instrument) => instrument.get('template')));
   },
 
   actions: {
