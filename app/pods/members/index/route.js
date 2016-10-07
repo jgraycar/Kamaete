a7b6313bb331909cbe082ef3b994fefe75bd73b8
Joel Graycar <jgraycar@berkeley.edu>
2016-10-07 02:24:43 -0700

    Add create fab to members index page

--- app/pods/members/index/route.js
+++ app/pods/members/index/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('member');
+  },
+
+  actions: {
+    create() {
+      Ember.$('#new-member-name').focus();
+    }
   }
 });
